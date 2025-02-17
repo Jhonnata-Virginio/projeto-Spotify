4b230d97be2cdbe70b843b8e19c5d55fdc185c2b
Jhonnata__Virginio <jhonnata.vieira.ext@synchro.com.br>
2025-02-17 16:09:16 -0300

    Finalizada as páginas Artists e Songs

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import SingleItem from './SingleItem.jsx'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+
+    const { pathname } = useLocation()
+    const isHome = pathname === "/"
+    const finalItems = isHome ? items : Infinity
 
     return (
         <div className='item-list'>
             <div className='item-list__header'>
                 <h2>{title} Populares</h2>
-                <Link className='item-list__link' to={path}>Mostrar tudo</Link>
+                {isHome ? (<Link className='item-list__link' to={path}>Mostrar tudo</Link>) : <></>}
+                
             </div>
 
             <div className="item-list__container">
-                {itemsArray.filter((currentValue, index) => index < items).map((currObj, index) => <SingleItem {...currObj} idPath={idPath} key={`${title}-${index}`}/>)}
+                {itemsArray.filter((currentValue, index) => index < finalItems).map((currObj, index) => <SingleItem {...currObj} idPath={idPath} key={`${title}-${index}`}/>)}
                 
             </div>    
         </div>
