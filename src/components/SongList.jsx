6c94b43cabd9d590a383de123b90195d831f7449
Jhonnata-Virginio <jhonnatavieiravirginio@gmail.com>
2025-02-18 10:33:58 -0300

    Estruturando as páginas Artist e Song

---
+++ src/components/SongList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SongItem from './SongItem'
+
+const SongList = () => {
+  return <div className="song-list">
+    <SongItem />
+
+    <p className='song-list__see-more'>Ver Mais</p>
+  </div>
+}
+
+export default SongList
--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Player from '../components/Player'
+import { Link } from 'react-router-dom'
 
 const Song = () => {
-  return (
-    <div>Song</div>
-  )
+  return <div className='song'>
+
+    <div className="song__container">
+      <div className="song__image-container">
+        <img className='song__image' src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="imagem da música x" />
+      </div>
+    </div>
+
+    <div className="song__bar">
+      <Link to="/artist/1" className="song__artist-image">
+        <img width={75} height={75} className='' src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf" alt="Imagem do artista X" />
+      </Link>
+
+      <Player/>
+
+      <div className="">
+        <p className='song__name'>Última Saudade - Ao vivo</p>
+        <p>Henrique & Juliano</p>
+      </div>
+      
+    </div>
+  </div>
 }
 
 export default Song
